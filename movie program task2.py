e9935883722d91897dc49585f46e1b1be0db4a80
Mohammedhasif <mdhasiftn@gmail.com>
2025-08-04 17:52:10 +0530

    Add files via upload

---
+++ movie program task2.py
@@ -0,0 +1,21 @@
+choice = input("What type of movie do you like? (action, romance, historical , horror): ")
+
+# Recommend a movie based on choice
+if choice == "action":
+    print("You can watch: thuppaki")
+if choice == "action":
+    print("You can watch: salar")
+elif choice == "romance":
+    print("You can watch: sachin")
+elif choice == "romance":
+   print("You can watch: love today")
+elif choice == "horror":
+    print("You can watch: aranmanai")
+elif choice == "horror":
+    print("You can watch: kanchana")
+elif choice == "historical":
+    print("You can watch: bahubali")
+elif choice == "historical":
+    print("You can watch: ponniyin selvan")
+else:
+  print("Sorry, no recommendation for that type.")
